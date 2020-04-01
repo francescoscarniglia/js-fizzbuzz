24892229b8c5b71880168930f2c28a69585905c1
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-04-01 17:20:41 +0200

    Ex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,20 @@
 console.log('js ok');
 // Scrivi un programma che stampi i numeri da 1 a 100, ma:
+
 var numStamp = Math.floor(Math.random() *100) +1;
-console.log(numStamp);
+// console.log(numStamp);
+
 // per i multipli di 3 stampi “Fizz” al posto del numero
-var fizz = (numStamp % 3);
-var res = fizz.replace(fizz, 'Fizz');
-console.log(res);
 
-
+for(var i = 0; i < 100; i++) {
+    if(numStamp % 3) {
+      console.log('Fizzi');
+    } else if(numStamp % 5) {
+      console.log('Buzz');
+    } else {
+      console.log('FizzBuzz');
+    }
+}
 
 // per i multipli di 5 stampi Buzz.
 // per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
